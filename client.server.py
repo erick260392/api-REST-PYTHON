80636366ae542bd61ae6b6411ba9102b54aa4fe0
erick260392 <erickliconamorales2016@gmail.com>
2022-12-03 00:08:15 -0600

    first commit

---
+++ client.server.py
@@ -0,0 +1,20 @@
+
+import requests
+
+ENDPOINT_POKEAPI = 'https://pokeapi.co/api/v2/pokemon/pikachu'
+
+response = requests.get(ENDPOINT_POKEAPI)
+#print(response.status_code)
+response = response.json()
+# print(response)
+
+abilities = response['abilities'][0]
+ability_name = abilities['ability'][ 'name']
+
+print(abilities)
+print(ability_name)
+
+if 'static' in ability_name:
+    print('Y')
+else:
+    print('N')
