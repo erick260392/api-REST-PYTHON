05993c261b1b28b30423d80a7ef254d93ea1e97a
erick260392 <erickliconamorales2016@gmail.com>
2022-12-05 22:24:05 -0600

    server flask

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask, request
-from flask_api import status
-from abilitys import ability
-from flask import  jsonify
+import requests
+
 
 app = Flask(__name__)
 
-@app.route('/ability', methods=["GET"])
-def getability():
-    
-    
-    return jsonify(ability["ability"]["name"])
-    
+
+def controller_poke(headers):
+    try:
+      end_point_poke_Api = headers['end_point_poke_Api']
+      ability_name = headers['ability_name']
+      
+      response = requests.get(end_point_poke_Api)
+      #print(response.status_code)
+      response = response.json()
+      print(response)
+       # print(response)
+       
+       
+      abilities = response['abilities'][0]
+      ability_name = abilities['ability'][ 'name']
+
+      print(abilities,ability_name)
+    except:
+        ...
+    else:
+        if 'static' in ability_name:
+             print('Y')
+        else:
+            print('N')
+                
+ 
+    return {'controller_poke': True}
+
+@app.route('/poke')
+def poke():
+        response = controller_poke(request.headers)
+        return response
 
 
 if __name__ == "__main__":
